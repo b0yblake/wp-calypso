ad1d438d4e93390475ae277cbec40cceb240ec18
Edwin Takahashi <egao@outlook.com>
2021-08-18 16:34:59 -0700

    Refactor how the `selectPlan` works in GutenboardingFlow. Add wait for initializer for Sidebar.

--- packages/calypso-e2e/src/lib/components/sidebar-component.ts
+++ packages/calypso-e2e/src/lib/components/sidebar-component.ts
@@ -119,6 +119,14 @@ export class SidebarComponent {
 	 * @returns {Promise<void>} No return value.
 	 */
 	async switchSite(): Promise< void > {
+		const viewportName = getViewportName();
+
+		await this.waitForSidebarInitialization();
+
+		if ( viewportName === 'mobile' ) {
+			await this._openMobileSidebar();
+		}
+
 		await this.page.click( ':text("Switch Site")' );
 		await this.page.waitForSelector( `${ selectors.layout }.focus-sites` );
 	}
